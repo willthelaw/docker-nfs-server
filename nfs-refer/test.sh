46b4e6f394d306e7ba63ad0d3fbbc29c0e203cb4
William Law <wlaw@stanford.edu>
2018-10-03 20:31:33 -0700

    Cleaned out test files

--- nfs-refer/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-nfslinks=`cut -f1 -d" " < exports-home | grep nfs | xargs`
-for i in nfslinks; do
-        if [ "$i" != "/nfs/data" ]; then
-                mkdir $i
-                mount -o bind $i $i
-        fi
-done
